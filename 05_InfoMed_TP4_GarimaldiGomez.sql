1b3b90720cba3ac64c84ba392167391d5fd0ff1b
RamiroGarimaldi <rgarimaldi@itba.edu.ar>
2025-05-26 18:55:13 -0300

    Add files via upload

--- 05_InfoMed_TP4_GarimaldiGomez.sql
+++ 05_InfoMed_TP4_GarimaldiGomez.sql
@@ -12,9 +12,5 @@ SET ciudad = 'Córdoba'
 WHERE LOWER(ciudad) IN ('cordoba', 'cordóva', 'córodba', 'córdoba');
 
 UPDATE Pacientes
-SET ciudad = 'Córdoba'
-WHERE LOWER(ciudad) IN ('cordoba', 'cordóva', 'córodba', 'córdoba');
-
-UPDATE Pacientes
 SET ciudad = 'Mendoza'
 WHERE LOWER(ciudad) IN ('mendoza', 'mendzoa');
